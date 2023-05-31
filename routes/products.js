4fab87a8be5868f244aa41e94411ecf08135614e
unknown <sushant.shekhar151997@gmail.com>
2023-06-01 00:03:38 +0530

    update category filter feature

--- routes/products.js
+++ routes/products.js
@@ -44,5 +44,31 @@ product.post("/add",async(req,res)=>{
     }
     
 })
+product.post("/filter",async(req,res)=>{
+    const {  category } = req.body.params;
+
+  try {
+    // Build the query object
+    const query = {};
+
+   
+
+    // Add the category filter
+    if (category && category.length > 0) {
+      query.category = { $in: category };
+    }
+
+    // Perform the search using the query object
+    const product = await productModel.find(query);
+
+    res.json({product:product});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+   
+  
+    
+})
 
 module.exports={product}
